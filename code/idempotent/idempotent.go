5a332362e9957ead30f848d29b7489c6b24f01ad
unknown <amarpitmalik204@gmail.com>
2024-12-15 00:12:29 +0530

    idempotent

--- code/idempotent/idempotent.go
+++ code/idempotent/idempotent.go
@@ -9,20 +9,29 @@ import (
 
 var IdempotentTracker = make(map[string]bool)
 
-func idempotent() {
-	http.HandleFunc("/idempptent", getPaginatedData)
+func Idempotent() {
+	http.HandleFunc("/idempotent", processRequest)
 	fmt.Println("Idempotent Server running on http://localhost:8082")
 	log.Fatal(http.ListenAndServe(":8082", nil))
 }
 
-func getPaginatedData(w http.ResponseWriter, r *http.Request) {
-	if r.
-	idempotentKey := r.Header.Get("idempotent_key")
+func processRequest(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
+		return
+	}
 
-	if _, exists := IdempotentTracker[idempotentKey]; exists {
+	idempotentKey := r.Header.Get("idempotent_key")
+	fmt.Println(idempotentKey, IdempotentTracker)
+	if _, exists := IdempotentTracker[idempotentKey]; !exists {
+		IdempotentTracker[idempotentKey] = true
 		w.Header().Set("Content-Type", "application/json")
 		w.WriteHeader(http.StatusOK)
-		json.NewEncoder(w).Encode("Is not present")
+		json.NewEncoder(w).Encode("Request Processed Successfully")
+	} else {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusConflict)
+		json.NewEncoder(w).Encode("Duplicate Request")
 	}
 
 }
--- code/main.go
+++ code/main.go
@@ -1,10 +1,13 @@
 package main
 
+import "main/idempotent"
+
 // uncomment to a specific system
 func main() {
 	// go loadbalancer.StartLoadBalancer()
 	// api.API()
 	//plugin.StatisticsPlugin()
 	//scaling.RunAgent()
+	idempotent.Idempotent()
 
 }
